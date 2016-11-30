15777fcee9827656daaf116be14eb4ff128be37d
lvapeab <lvapeab@gmail.com>
2016-11-30 18:16:54 +0100

    Updated config

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ def load_parameters():
     '''
 
     # Input data params
-    DATASET_NAME = 'ue'                        # Task name
+    DATASET_NAME = 'EuTrans'                        # Task name
     SRC_LAN = 'es'                                  # Language of the source text
     TRG_LAN = 'en'                                  # Language of the target text
-    DATA_ROOT_PATH = '/media/HDD_2TB/DATASETS/%s/' % DATASET_NAME  # Path where data is stored
+    DATA_ROOT_PATH = 'examples/%s/' % DATASET_NAME  # Path where data is stored
 
     # SRC_LAN or TRG_LAN will be added to the file names
     TEXT_FILES = {'train': 'DATA/training.',        # Data files
@@ -25,7 +25,7 @@ def load_parameters():
     METRICS = ['coco']                            # Metric used for evaluating the model
     EVAL_ON_SETS = ['val']                        # Possible values: 'train', 'val' and 'test' (external evaluator)
     EVAL_ON_SETS_KERAS = []                       # Possible values: 'train', 'val' and 'test' (Keras' evaluator). Untested.
-    START_EVAL_ON_EPOCH = 10                       # First epoch where the model will be evaluated
+    START_EVAL_ON_EPOCH = 1                       # First epoch where the model will be evaluated
     EVAL_EACH_EPOCHS = True                       # Select whether evaluate between N epochs or N updates
     EVAL_EACH = 1                                 # Sets the evaluation frequency (epochs or updates)
 
@@ -42,7 +42,7 @@ def load_parameters():
     SAMPLE_ON_SETS = ['train', 'val']             # Possible values: 'train', 'val' and 'test'
     N_SAMPLES = 5                                 # Number of samples generated
     START_SAMPLING_ON_EPOCH = 1                   # First epoch where to start the sampling counter
-    SAMPLE_EACH_UPDATES = 2500                     # Sampling frequency (always in #updates)
+    SAMPLE_EACH_UPDATES = 500                     # Sampling frequency (always in #updates)
 
     #TODO: WIP! Other subword methods?
     POS_UNK = False
