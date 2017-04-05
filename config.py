2f9b6bfb4081b159165993ce4017d8b3b47c76ac
Álvaro Peris <lvapeab@gmail.com>
2017-04-05 12:02:50 +0200

    LSTM usually perform better than GRU
    According to (https://arxiv.org/abs/1703.03906)

--- config.py
+++ config.py
@@ -120,7 +120,7 @@ def load_parameters():
 
     # Model parameters
     MODEL_TYPE = 'GroundHogModel'                 # Model to train. See model_zoo() for the supported architectures
-    RNN_TYPE = 'GRU'                              # RNN unit type ('LSTM' and 'GRU' supported)
+    RNN_TYPE = 'LSTM'                             # RNN unit type ('LSTM' and 'GRU' supported)
     INIT_FUNCTION = 'glorot_uniform'              # Initialization function for matrices (see keras/initializations.py)
 
     SOURCE_TEXT_EMBEDDING_SIZE = 420              # Source language word embedding size.
