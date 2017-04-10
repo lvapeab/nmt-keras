7c4a1f10f87224293e5d6c8e1f2c8eea7b181b80
Alvaro <lvapeab@gmail.com>
2017-04-10 12:56:30 +0200

    OL exploration scripts

--- explore_online_params.sh
+++ explore_online_params.sh
@@ -12,22 +12,22 @@ verbose=1
 
 mkdir -p  $dest_dir
 
-for algo in SGD Adagrad Adadelta Adam; do
-    for lr in 1.0 0.1 0.01 0.001 0.0001 0.00001 0.000001 ; do
-	echo " `date` Algo: ${algo}. LR: ${lr}"
-	echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
-	python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
-	       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
-	       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
-	       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
-	       --models ${model_path}/${model_files} -o -v ${verbose} \
-	       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
-	sleep 3
-    done
-done
+#for algo in SGD Adagrad Adadelta Adam; do
+#    for lr in 1.0 0.1 0.01 0.001 0.0001 0.00001 0.000001 ; do
+#	echo " `date` Algo: ${algo}. LR: ${lr}"
+#	echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+#	python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+#	       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+#	       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+#	       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+#	       --models ${model_path}/${model_files} -o -v ${verbose} \
+#	       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+#	sleep 3
+#    done
+#done
 
 for algo in PAS PPAS ; do
-    for lr in 1.0 0.1 0.01 0.001 0.0001 0.00001 0.000001 ; do
+    for lr in 0.01 0.001 0.0001 0.00001 0.000001 ; do
 	for c in 1.0  0.1 0.01 0.001 0.0001 0.00001 0.000001 ; do
             echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
             echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
