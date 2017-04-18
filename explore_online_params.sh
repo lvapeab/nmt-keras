07bc42e064627dac4f19c87a2295e1c1a3bce06e
lvapeab <lvapeab@gmail.com>
2017-04-18 10:17:11 +0200

    Removed OL exploration scripts

--- explore_online_params.sh
+++ explore_online_params.sh
@@ -1,43 +1,335 @@
 #!/usr/bin/env bash
 
-task=europarl
+task=xerox
 src_lan=en
 trg_lan=fr
-split=dev
-base_model_path=/media/HDD_2TB/MODELS/${task}/trained_models
-model_path=${base_model_path}/${task}_${src_lan}${trg_lan}_GroundHogModel_src_emb_512_bidir_True_enc_LSTM_512_dec_LSTM_512_deepout_linear_trg_emb_512_Adadelta_1.0joint_bpe
-model_files=update_300000
+split=test
+base_model_path=/media/HDD_2TB/MODELS/${task}/${src_lan}${trg_lan}/trained_models
+model_path=${base_model_path}
+model_files=epoch_39
 dest_dir=Online_learning_experiments/$task/${src_lan}${trg_lan}/
 verbose=1
 
 mkdir -p  $dest_dir
 
-#for algo in SGD Adagrad Adadelta Adam; do
-#    for lr in 1.0 0.1 0.01 0.001 0.0001 0.00001 0.000001 ; do
-#	echo " `date` Algo: ${algo}. LR: ${lr}"
-#	echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
-#	python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
-#	       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
-#	       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
-#	       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
-#	       --models ${model_path}/${model_files} -o -v ${verbose} \
-#	       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
-#	sleep 3
-#    done
-#done
 
-for algo in PAS PPAS ; do
-    for lr in 0.01 0.001 0.0001 0.00001 0.000001 ; do
-	for c in 1.0  0.1 0.01 0.001 0.0001 0.00001 0.000001 ; do
-            echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
-            echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
-            python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
-                   -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
-                   -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
-                   --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
-                   --models ${model_path}/${model_files} -o -v ${verbose} \
-                   --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
-            sleep 3
-        done
-    done
-done
+
+
+
+algo=SGD
+lr=0.001
+
+echo " `date` Algo: ${algo}. LR: ${lr}."
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+
+
+
+algo=Adagrad
+lr=0.001
+
+echo " `date` Algo: ${algo}. LR: ${lr}."
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+
+
+
+algo=Adagrad
+lr=0.0001
+
+echo " `date` Algo: ${algo}. LR: ${lr}."
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+
+
+
+
+algo=Adadelta
+lr=0.1
+
+echo " `date` Algo: ${algo}. LR: ${lr}."
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+
+
+
+
+algo=Adadelta
+lr=0.01
+
+echo " `date` Algo: ${algo}. LR: ${lr}."
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+
+
+
+
+
+algo=Adam
+lr=0.001
+
+echo " `date` Algo: ${algo}. LR: ${lr}."
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+
+
+
+
+
+algo=Adam
+lr=0.0001
+
+echo " `date` Algo: ${algo}. LR: ${lr}."
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+
+
+
+
+algo=Adam
+lr=0.00001
+
+echo " `date` Algo: ${algo}. LR: ${lr}."
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr} 2>&1
+
+
+
+
+algo=PAS
+lr=0.01
+c=0.01
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+algo=PAS
+lr=0.001
+c=0.001
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+
+
+
+
+
+
+algo=PAS
+lr=0.01
+c=0.1
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+algo=PAS
+lr=0.01
+c=1.
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+
+
+
+
+
+
+algo=PAS
+lr=0.001
+c=1
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+
+
+
+
+
+
+algo=PPAS
+lr=0.01
+c=0.01
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+
+
+
+
+
+
+algo=PPAS
+lr=0.001
+c=0.01
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+
+
+
+
+
+
+algo=PPAS
+lr=0.001
+c=0.001
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+
+algo=PPAS
+lr=0.001
+c=1
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+
+
+
+
+
+
+algo=PPAS
+lr=0.0001
+c=0.0001
+
+echo " `date` Algo: ${algo}. LR: ${lr}. C: ${c}"
+echo -e "\t Storing log in ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c}"
+python main.py --config ${model_path}/config.pkl --dataset datasets/Dataset_${task}_${src_lan}${trg_lan}.pkl \
+       -src /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${src_lan}  \
+       -trg /media/HDD_2TB/DATASETS/${task}/${src_lan}${trg_lan}/${split}.${trg_lan} \
+       --hypotheses ${dest_dir}/hyps.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} \
+       --models ${model_path}/${model_files} -o -v ${verbose} \
+       --changes LR=${lr} OPTIMIZER=${algo} C=${c} > ${dest_dir}/log.${task}_${src_lan}${trg_lan}.${algo}.${lr}.${c} 2>&1
+sleep 3
+
+
+
+
+
