a29b34dbb62e47da56002daaa94778ea3c606945
lvapeab <lvapeab@gmail.com>
2017-06-01 10:35:37 +0200

    Update main function to match master

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def train_model(params):
+def train_model(params, load_dataset=None):
     """
     Training function. Sets the training parameters from params. Build or loads the model and launches the training.
     :param params: Dictionary of network hyperparameters.
@@ -54,7 +54,11 @@ def train_model(params):
     check_params(params)
 
     # Load data
-    dataset = build_dataset(params)
+    if load_dataset is None:
+        dataset = build_dataset(params)
+    else:
+        dataset = loadDataset(load_dataset)
+
     params['INPUT_VOCABULARY_SIZE'] = dataset.vocabulary_len[params['INPUTS_IDS_DATASET'][0]]
     params['OUTPUT_VOCABULARY_SIZE'] = dataset.vocabulary_len[params['OUTPUTS_IDS_DATASET'][0]]
 
@@ -108,6 +112,8 @@ def train_model(params):
         nmt_model = updateModel(nmt_model, params['STORE_PATH'], params['RELOAD'], reload_epoch=params['RELOAD_EPOCH'])
         nmt_model.setParams(params)
         nmt_model.setOptimizer()
+        params['EPOCH_OFFSET'] = params['RELOAD'] if params['RELOAD_EPOCH'] else \
+            int(params['RELOAD'] * params['BATCH_SIZE'] / dataset.len_train)
 
     # Callbacks
     callbacks = buildCallbacks(params, nmt_model, dataset)
@@ -118,12 +124,18 @@ def train_model(params):
     logging.debug('Starting training!')
     training_params = {'n_epochs': params['MAX_EPOCH'],
                        'batch_size': params['BATCH_SIZE'],
-                       'homogeneous_batches': params['HOMOGENEOUS_BATCHES'], 'maxlen': params['MAX_OUTPUT_TEXT_LEN'],
+                       'homogeneous_batches': params['HOMOGENEOUS_BATCHES'],
+                       'maxlen': params['MAX_OUTPUT_TEXT_LEN'],
                        'joint_batches': params['JOINT_BATCHES'],
-                       'lr_decay': params['LR_DECAY'], 'lr_gamma': params['LR_GAMMA'],
-                       'epochs_for_save': params['EPOCHS_FOR_SAVE'], 'verbose': params['VERBOSE'],
-                       'eval_on_sets': params['EVAL_ON_SETS_KERAS'], 'n_parallel_loaders': params['PARALLEL_LOADERS'],
-                       'extra_callbacks': callbacks, 'reload_epoch': params['RELOAD'], 'epoch_offset': params['RELOAD'],
+                       'lr_decay': params['LR_DECAY'],
+                       'lr_gamma': params['LR_GAMMA'],
+                       'epochs_for_save': params['EPOCHS_FOR_SAVE'],
+                       'verbose': params['VERBOSE'],
+                       'eval_on_sets': params['EVAL_ON_SETS_KERAS'],
+                       'n_parallel_loaders': params['PARALLEL_LOADERS'],
+                       'extra_callbacks': callbacks,
+                       'reload_epoch': params['RELOAD'],
+                       'epoch_offset': params.get('EPOCH_OFFSET', 0),
                        'data_augmentation': params['DATA_AUGMENTATION'],
                        'patience': params.get('PATIENCE', 0),  # early stopping parameters
                        'metric_check': params.get('STOP_METRIC', None) if params.get('EARLY_STOP', False) else None,
@@ -553,10 +565,9 @@ if __name__ == "__main__":
 
     elif parameters['MODE'] == 'training':
         logging.info('Running training.')
-        train_model(parameters)
+        train_model(parameters, args.dataset)
     elif parameters['MODE'] == 'sampling':
         logging.info('Running sampling.')
         apply_NMT_model(parameters)
 
     logging.info('Done!')
-
