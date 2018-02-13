24d5d52d330d7a3123b290921f3c51d681aea87c
lvapeab <lvapeab@gmail.com>
2018-02-13 10:55:21 +0100

    Break deep models into unidirectional and bidirecionals (memory issues in travis.org)

--- tests/test_NMT_architectures/unidir_deep_GRU_ConditionalGRU.py
+++ tests/test_NMT_architectures/unidir_deep_GRU_ConditionalGRU.py
@@ -35,64 +35,15 @@ def load_tests_params():
     return params
 
 
-def test_NMT_Bidir_deep_GRU_ConditionalLSTM():
+def test_NMT_Unidir_deep_GRU_ConditionalGRU():
     params = load_tests_params()
 
-    # Current test params: Single layered GRU - GRU
-    params['BIDIRECTIONAL_ENCODER'] = True
-    params['N_LAYERS_ENCODER'] = 2
-    params['BIDIRECTIONAL_DEEP_ENCODER'] = False
-    params['ENCODER_RNN_TYPE'] = 'GRU'
-    params['DECODER_RNN_TYPE'] = 'ConditionalLSTM'
-    params['N_LAYERS_DECODER'] = 2
-
-    params['REBUILD_DATASET'] = True
-    dataset = build_dataset(params)
-    params['INPUT_VOCABULARY_SIZE'] = dataset.vocabulary_len[params['INPUTS_IDS_DATASET'][0]]
-    params['OUTPUT_VOCABULARY_SIZE'] = dataset.vocabulary_len[params['OUTPUTS_IDS_DATASET'][0]]
-    params['MODEL_NAME'] = \
-        params['TASK_NAME'] + '_' + params['SRC_LAN'] + params['TRG_LAN'] + '_' + params['MODEL_TYPE'] + \
-        '_src_emb_' + str(params['SOURCE_TEXT_EMBEDDING_SIZE']) + \
-        '_bidir_' + str(params['BIDIRECTIONAL_ENCODER']) + \
-        '_enc_' + params['ENCODER_RNN_TYPE'] + '_*' + str(params['N_LAYERS_ENCODER']) + '_' + str(
-            params['ENCODER_HIDDEN_SIZE']) + \
-        '_dec_' + params['DECODER_RNN_TYPE'] + '_*' + str(params['N_LAYERS_DECODER']) + '_' + str(
-            params['DECODER_HIDDEN_SIZE']) + \
-        '_deepout_' + '_'.join([layer[0] for layer in params['DEEP_OUTPUT_LAYERS']]) + \
-        '_trg_emb_' + str(params['TARGET_TEXT_EMBEDDING_SIZE']) + \
-        '_' + params['OPTIMIZER'] + '_' + str(params['LR'])
-    params['STORE_PATH'] = K.backend() + '_test_train_models/' + params['MODEL_NAME'] + '/'
-
-    # Test several NMT-Keras utilities: train, sample, sample_ensemble, score_corpus...
-    train_model(params)
-    params['RELOAD'] = 2
-    apply_NMT_model(params)
-    parser = argparse.ArgumentParser('Parser for unit testing')
-    parser.dataset = params['DATASET_STORE_PATH'] + '/Dataset_' + params['DATASET_NAME'] + '_' + params['SRC_LAN'] + params['TRG_LAN'] + '.pkl'
-
-    parser.text = params['DATA_ROOT_PATH'] + '/' + params['TEXT_FILES']['val'] + params['SRC_LAN']
-    parser.splits = ['val']
-    parser.config = params['STORE_PATH'] + '/config.pkl'
-    parser.models = [params['STORE_PATH'] + '/epoch_' + str(2)]
-    parser.verbose = 0
-    parser.dest = None
-    parser.source = params['DATA_ROOT_PATH'] + '/' + params['TEXT_FILES']['val'] + params['SRC_LAN']
-    parser.target = params['DATA_ROOT_PATH'] + '/' + params['TEXT_FILES']['val'] + params['TRG_LAN']
-    for n_best in [True, False]:
-        parser.n_best = n_best
-        sample_ensemble(parser, params)
-    score_corpus(parser, params)
-
-
-def test_NMT_Unidir_deep_GRU_ConditionalLSTM():
-    params = load_tests_params()
-
-    # Current test params: Single layered GRU - GRU
+    # Current test params: Two-layered GRU - ConditionalGRU
     params['BIDIRECTIONAL_ENCODER'] = False
     params['N_LAYERS_ENCODER'] = 2
     params['BIDIRECTIONAL_DEEP_ENCODER'] = False
     params['ENCODER_RNN_TYPE'] = 'GRU'
-    params['DECODER_RNN_TYPE'] = 'ConditionalLSTM'
+    params['DECODER_RNN_TYPE'] = 'ConditionalGRU'
     params['N_LAYERS_DECODER'] = 2
 
     params['REBUILD_DATASET'] = True
